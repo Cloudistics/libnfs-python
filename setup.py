5f3c553570ddba58795855dc931a7261ba0d15a9
Getnat Ejigu <getnat.ejigu@cloudistics.com>
2020-10-07 12:44:27 -0400

    Fixes: - Check error code if mount failed - Raise exceptions when call fail with error - Build for Python3 - Commented out couple functions because it fails build

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
-from os import environ
 
+# Update: 10/7/2020
+#   - Package name
+#   - Change release and version number
+
+
+from os import environ
 try:
     from setuptools import setup, Extension
     from setuptools.command.build_ext import build_ext
@@ -11,7 +16,7 @@ except ImportError:
 # Although setuptools supports swig, it fails to copy the generated
 # Python wrapper: http://stackoverflow.com/questions/12491328
 
-cmdclass = { }
+cmdclass = {}
 
 try:
     from setuptools.command.install import install
@@ -24,54 +29,61 @@ else:
             install.run(self)
     cmdclass['install'] = Install
 
+
 from distutils.command.build import build
+
 
 class Build(build):
     def run(self):
         self.run_command('build_ext')
         build.run(self)
+
+
 cmdclass['build'] = Build
 
-name = 'libnfs'
+
+name = 'dcc-libnfs-python'
 version = '1.0'
-release = '4'
-versrel = version + '-' + release
+release = '5'
+versrel = version + '.' + release
 readme = 'README'
 download_url = "https://github.com/sahlberg/libnfs-python/libnfs-" + \
                                                           versrel + ".tar.gz"
 with open(readme, "r") as f:
     long_description = f.read()
 
-_libnfs = Extension(name='libnfs._libnfs',
-                   sources=['libnfs/libnfs.i'],
-                   swig_opts=['-shadow', '-threads'],
-                   extra_link_args=['-g'],
-                   extra_compile_args=['-g'],
-                   libraries=['nfs'],
+_libnfs = Extension(
+    name='libnfs._libnfs',
+    sources=['libnfs/libnfs.i'],
+    swig_opts=['-shadow', '-threads'],
+    extra_link_args=['-g'],
+    extra_compile_args=['-g'],
+    libraries=['nfs'],
 )
 
 
-setup(name = name,
-      version = versrel,
-      description = 'NFS client for Python.',
-      long_description = long_description,
-      license = 'LGPLv2.1',
-      platforms = ['any'],
-      author = 'Ronnie Sahlberg',
-      author_email = 'ronniesahlberg@gmail.com',
-      url = 'https://github.com/sahlberg/libnfs-python/',
-      download_url = download_url,
-      packages = ['libnfs'],
-      classifiers = [
-          'Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'Operating System :: OS Independent',
-          'Programming Language :: C',
-          'Programming Language :: Python',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'Topic :: Communications :: File Sharing',
-          'Topic :: System :: Filesystems',
-      ],
-      ext_modules = [_libnfs],
-      cmdclass = cmdclass,
-      )
+setup(
+    name=name,
+    version=versrel,
+    description='NFS client for Python.',
+    long_description=long_description,
+    license='LGPLv2.1',
+    platforms=['any'],
+    author='Ronnie Sahlberg',
+    author_email='ronniesahlberg@gmail.com',
+    url='https://github.com/sahlberg/libnfs-python/',
+    download_url=download_url,
+    packages=['libnfs'],
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Operating System :: OS Independent',
+      'Programming Language :: C',
+      'Programming Language :: Python',
+      'Topic :: Software Development :: Libraries :: Python Modules',
+      'Topic :: Communications :: File Sharing',
+      'Topic :: System :: Filesystems',
+    ],
+    ext_modules=[_libnfs],
+    cmdclass=cmdclass,
+)
--- utils/nfs-ls.py
+++ utils/nfs-ls.py
@@ -2,27 +2,32 @@
 # coding: utf-8
 #
 # Example program to list content of NFS directory
+#
+# Update: 10/7/2020
+#   - Update print statement to a function call to comply with Python 3
+#     syntax.
 
 import sys
-
 import libnfs
 
+
 def usage():
-    print 'Usage: nfs-ls.py <NFS-URL>'
-    print ''
-    print 'Example: nfs-ls.py nfs://127.0.0.1/data'
+    print('Usage: nfs-ls.py <NFS-URL>')
+    print('')
+    print('Example: nfs-ls.py nfs://127.0.0.1/data')
     sys.exit()
+
 
 def ls(dir):
     import libnfs
 
-    nfs = libnfs.NFS(dir) 
-    print 'nfs', nfs
+    nfs = libnfs.NFS(dir)
+    print('nfs', nfs)
     for ent in nfs.listdir("."):
         if ent in ['.', '..']:
             continue
         st = nfs.lstat(ent)
-        print ent, st
+        print(ent, st)
 
 
 if __name__ == "__main__":
